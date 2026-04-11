7331e573c061077c1b5a41655b70d41c6b6803cb
xwysez <brutaslouise@gmail.com>
2026-04-11 19:38:06 +0800

    forebase-update 1.2

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCQVbMpW5CRZJHLBZmN5yH4uygR7mUSwb4",
-  authDomain: "mitos-pensionne.firebaseapp.com",
-  projectId: "mitos-pensionne",
-  storageBucket: "mitos-pensionne.firebasestorage.app",
-  messagingSenderId: "63535680420",
-  appId: "1:63535680420:web:2ab864afc876492f410f31"
-};
